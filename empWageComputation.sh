01ffb572892ce62d8ad48d2e48e1f8213f0f2eba
dbasumatary <basudhondip@gmail.com>
2023-01-18 22:05:00 +0530

    Refactor the Code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,30 +9,35 @@ empRatePerHr=20;
 numWorkingDays=20;
 day=1;
 
+function calculateWorkingHour(){
+	case $randomCheck in 
+		     $isFullTime)
+                        empHrs=8;;
+                     $isPartTime)
+                        empHrs=4;;
+                     *)
+                        empHrs=0;;
+esac
+	echo $empHrs;
+}
+
 while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 
 do
 randomCheck=$((RANDOM%3));
 
-case $randomCheck in $isFullTime)
-			empHrs=8;;
-		     $isPartTime)
-			empHrs=4;;
-		     *)
-			empHrs=0;;
-esac
+	wHour=$(calculateWorkingHour $randomCheck);
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
 
-totalWorkingHour=$(($totalWorkingHour+$empHrs))
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour-$wHour))
+		break;
+	fi
 
-if [ $totalWorkingHour -gt 100 ]
-then
-totalWorkingHour=$(($totalWorkingHour-$empHrs))
-break;
-fi
-
-salary=$(($empHrs*$empRatePerHr))
-totalSalary=$(($totalSalary+$salary))
-((day++))
+	salary=$(($wHour*$empRatePerHr))
+	totalSalary=$(($totalSalary+$salary))
+	((day++));
 done
 
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour";
