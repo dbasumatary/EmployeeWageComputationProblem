a4b6b96d6aa5b2ea881c47b7dedd0307032513de
dbasumatary <basudhondip@gmail.com>
2023-01-18 20:04:30 +0530

    Add: Part time Employee & Wage - Assume Part time Hour is 4

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,23 @@
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20
 
-if [ $isPresent -eq $randomCheck ]
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ]
 then
 
-        echo "Employee is present"
-	empRatePerHr=20
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr))
-	echo "salary = "$salary
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+then
+	empHrs=4;
 else
-
-        echo "Employee is absent"
-	echo "salary = "0
+	empHrs=0;
 fi
+
+        salary=$(($empHrs*$empRatePerHr))
+	echo "salary = $salary"
+
 
